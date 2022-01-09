4f02cc07de4d913898e61d7a702070cace1f6bd0
sbahadirm <sbahadirm@gmail.com>
2022-01-09 10:10:58 +0300

    changed navbar

--- src/component/menu/Menu.js
+++ src/component/menu/Menu.js
@@ -6,37 +6,31 @@ import CategoryMenus from "./CategoryMenus";
 class Menu extends React.Component {
 
     render() {
-        return <div className="col-md-6 offset-md-3">
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="#">N11</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+        return <div className="col-md-6 offset-md-3 ">
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="/">N11</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="/list">Tüm Ürünler</Nav.Link>
 
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
+                            <CategoryMenus></CategoryMenus>
 
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Link</a>
-                        </li>
+                            <NavDropdown title="Hesabım" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="/basket">Sepete Git</NavDropdown.Item>
+                                <NavDropdown.Item href="/orders">Önceki Siparişler</NavDropdown.Item>
+                                <NavDropdown.Item href="/profile/:username">Kullanıcı Bilgileri</NavDropdown.Item>
+                                <NavDropdown.Item href="/favorylist">Favori Listeleri</NavDropdown.Item>
+                                <NavDropdown.Item href="/product/add">Ürün Ekle</NavDropdown.Item>
 
-                        <CategoryMenus></CategoryMenus>
-
-                        <NavDropdown title="Hesabım" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="/basket">Sepete Git</NavDropdown.Item>
-                            <NavDropdown.Item href="/orders">Önceki Siparişler</NavDropdown.Item>
-                            <NavDropdown.Item href="/profile/:username">Kullanıcı Bilgileri</NavDropdown.Item>
-                            <NavDropdown.Item href="/favorylist">Favori Listeleri</NavDropdown.Item>
-                            <NavDropdown.Item href="/product/add">Ürün Ekle</NavDropdown.Item>
-
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="/logout">Çıkış</NavDropdown.Item>
-                        </NavDropdown>
-
-                    </ul>
-
-                </div>
-            </nav>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="/logout">Çıkış</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
 
 
